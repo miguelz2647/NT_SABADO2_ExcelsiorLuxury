c78be3975ac0be8871ffac3fd51ef2513b008320
Miguel Zapata <miguel.zapata@omc.com>
2026-04-13 12:12:31 -0500

    agrege la carpeta data y el requeriments

--- utils/simulador_Usuarios.py
+++ utils/simulador_Usuarios.py
@@ -11,12 +11,26 @@ def simular_Usuarios(num_usuarios):
     usuarios = []
     for _ in range(num_usuarios):
       
-      servicios={
+      usuario={
          "Id": random.randint(1, 1000),
          "Nombres": random.choice(listaNombres),
          "Apellidos": random.choice(listaApellidos),
          "Email": random.choice(listaEmails),
          "Contraseña": random.choice(listContraseñas),         
       }
-    usuarios.append(servicios)
+
+      probabilodadError=random.random()
+
+      if probabilodadError<0.1:
+            usuario["Id"]=random.choice([None,-1,0])
+            usuario["Nombres"]=None
+      elif probabilodadError<0.3:
+            usuario["Apellidos"]=" "+usuario["Apellidos"]+" "
+      elif probabilodadError<0.6:
+            usuario["Email"]=usuario["Email"].upper()
+      elif probabilodadError<0.9:
+            usuario["Contraseña"]=None
+
+
+      usuarios.append(usuario)
     return usuarios
